126786f301bdd1fdce987fefee42c11a1e8987d4
Philip Van Raalte <phil.vanraalte@gmail.com>
2017-06-21 14:24:50 -0400

    modified BinarySearch

--- src/myMethods/BinarySearch.java
+++ src/myMethods/BinarySearch.java
@@ -19,7 +19,32 @@ public class BinarySearch {
         
         while(high >= low){
             //get the middle of the list
-            int middle = (low+high) / 2;
+            int middle = (int) Math.floor((low+high) / 2);
+            if(intArray[middle] == key){
+                return true;
+            }
+            //if the value in the middle is less then the key
+            //increase the lowest point of the list to search
+            else if(intArray[middle] < key){
+                low = middle + 1;
+            }
+            //if the value in the middle is more then the key
+            //decrease the highest point of the list to search
+            else if(intArray[middle] > key){
+                high = middle - 1;
+            }
+        }
+        return false;
+    }
+    
+    public static boolean exists(double[] intArray, double key){
+        Arrays.sort(intArray);
+        int low = 0; //beginning of list to search
+        int high = intArray.length; //end of list to search
+        
+        while(high >= low){
+            //get the middle of the list
+            int middle = (int) Math.floor((low+high) / 2);
             if(intArray[middle] == key){
                 return true;
             }
